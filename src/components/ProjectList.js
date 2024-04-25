78bc6773167abad628a74c6ce49a12c32908a251
Stephen Ochieng <materiamystic@gmail.com>
2024-04-25 23:36:50 +0300

    Adds the required lists and keys

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,14 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  const recordsArray = projects.map((record) => {
+    return <ProjectItem key={record.id} record={record} />;
+  });
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{recordsArray}</div>
     </div>
   );
 }
